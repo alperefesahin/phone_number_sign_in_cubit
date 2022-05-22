1cc705e091dafe7b60bf618ba36bad0909b4eb3c
Alper Efe Şahin <efe.mate@gmail.com>
2022-05-23 00:01:17 +0300

    firebase setup

--- lib/generated_plugin_registrant.dart
+++ lib/generated_plugin_registrant.dart
@@ -6,12 +6,14 @@
 // ignore_for_file: lines_longer_than_80_chars
 // ignore_for_file: depend_on_referenced_packages
 
+import 'package:firebase_core_web/firebase_core_web.dart';
 import 'package:libphonenumber_web/libphonenumber_web.dart';
 
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 // ignore: public_member_api_docs
 void registerPlugins(Registrar registrar) {
+  FirebaseCoreWeb.registerWith(registrar);
   LibPhoneNumberPlugin.registerWith(registrar);
   registrar.registerMessageHandler();
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:phone_number_sign_in/firebase_options.dart';
 import 'package:phone_number_sign_in/presentation/core/app_widget.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const AppWidget());
 }
