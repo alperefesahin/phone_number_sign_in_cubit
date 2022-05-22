b6e83021564b6c09add23fb70fb27b442deeba58
Alper Efe Şahin <efe.mate@gmail.com>
2022-05-22 15:27:48 +0300

    initial commit

---
+++ lib/presentation/core/app_widget.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:phone_number_sign_in/presentation/pages/sign_in_page/sign_in_page.dart';
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Phone Number Sign-In',
+      home: SignInPage(),
+    );
+  }
+}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,12 +8,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:phone_number_sign_in/main.dart';
+import 'package:phone_number_sign_in/presentation/core/app_widget.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const AppWidget());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
